6d1d2b5d93dcdbdc9d43bdc04b8d7515bc409837
Kévin Lionnet <24947582+Cemus@users.noreply.github.com>
2024-11-12 09:47:45 +0100

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  build: {
+    target: "es2022",
+  },
+  esbuild: {
+    target: "es2022",
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      target: "es2022",
+    },
+  },
+  base: "/boilerplate/",
+});
